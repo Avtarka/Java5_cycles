213dd83938f2b1dcb8d4f1f4e8f4b80685b0ba69
Pavel Kryuchkov <mr.p@bk.ru>
2023-03-27 22:45:11 +0400

    Edit1

--- src/main/java/ru/netology/services/TimeManagementService.java
+++ src/main/java/ru/netology/services/TimeManagementService.java
@@ -1,14 +1,29 @@
 package ru.netology.services;
+
 public class TimeManagementService {
-    public long calculate(long income, long expenses) {
+//    public long calculate(long income, long expenses) {
+//       int count = 0; // счётчик месяцев отдыха
+//        long treshold = 0;
+//       for (int month = 0; month < 12; month++) {
+//           if (treshold >= 20_000) { // можем ли отдыхать?
+//                count++; // увеличиваем счётчик месяцев отдыха
+//                treshold = (treshold - expenses) - treshold / 3 * 2;
+//            } else {
+//                treshold = treshold + income - expenses;
+//            }
+//        }
+//        return count;
+//   }
+
+    public long calculate(int income, int expenses, int treshold) {
         int count = 0; // счётчик месяцев отдыха
-        long treshold = 0;
+        int cashAccount = 0;  // денег на счету
         for (int month = 0; month < 12; month++) {
-            if (treshold >= 20_000) { // можем ли отдыхать?
+            if (cashAccount >= treshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                treshold = (treshold - expenses) - treshold / 3 * 2;
+                cashAccount = (cashAccount - expenses) / 3;
             } else {
-                treshold = treshold + income - expenses;
+                cashAccount = cashAccount + income - expenses;
             }
         }
         return count;
--- src/test/java/ru/netology/services/TimeManagementServiceTest.java
+++ src/test/java/ru/netology/services/TimeManagementServiceTest.java
@@ -7,37 +7,36 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 
-
 public class TimeManagementServiceTest {
     @ParameterizedTest
 
-    @CsvFileSource(files="src/test/resources/TimeDate.csv")
-    public void testCalculationVocationMonths(long income, long expenses, int expected) {
+    @CsvFileSource(files = "src/test/resources/TimeDate.csv")
+    public void testCalculationVocationMonths(int income, int expenses, int treshold, int expected) {
 
         TimeManagementService service = new TimeManagementService();
 
-        long actual = service.calculate(income, expenses);
+        long actual = service.calculate(income, expenses, treshold);
 
 
         Assertions.assertEquals(expected, actual);
     }
 
 
-    @Test
-    public void testCalculationVocationMonths1() {
-        TimeManagementService service = new TimeManagementService();
+    //  @Test
+    //  public void testCalculationVocationMonths1() {
+    //      TimeManagementService service = new TimeManagementService();
 
-        // подготавливаем данные:
-        long income = 10_000;
-        long expenses = 3_000;
+    //       // подготавливаем данные:
+    //       int income = 10000;
+    //       int expenses = 3000;
+    //       int treshold = 20000;
+    //       int expected = 3;
 
-        int expected = 3;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(income, expenses);
+    //       // вызываем целевой метод:
+    //      long actual = service.calculate(income, expenses, treshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
+    //       Assertions.assertEquals(expected, actual);
+    // }
 
 }
